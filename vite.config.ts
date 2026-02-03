f24e21cc8447f5da3e80c1e51931a9cf1f03a503
ZhugeBane <j4cinto.jr@gmail.com>
2026-02-03 18:49:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // Isso ajuda a ignorar erros pequenos de TypeScript para garantir o build
-    typescript: {
-      ignoreBuildErrors: true,
+  resolve: {
+    alias: {
+      src: "/src",
     },
   },
-});
+})
